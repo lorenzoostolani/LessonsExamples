3be7211424cbf53e0a0cb7dc9da0f34410607be1
lorenzo.ostolani <lorenzo.ostolani@PC-4606>
2025-10-08 10:42:46 +0200

    Aggiungere i file di progetto.

---
+++ test/BlaisePascal.LessonsExamples.Domain.UnitTest/EnemyTest.cs
@@ -0,0 +1,34 @@
+namespace BlaisePascal.LessonsExamples.Domain.UnitTest
+{
+    public class EnemyTest
+    {
+        [Fact]
+        public void EnemyName_WhenTheNameIsValid_NameMustBeAssignedCorrectly()
+        {
+            //Arrange
+            Enemy newEnemy = new Enemy();
+
+            //Actit
+            newEnemy.SetName("Stefano");
+
+            //Assert
+            Assert.Equal("Stefano", newEnemy.GetName());
+
+
+        }
+
+        [Fact]
+        public void EnemyName_TheNameCannotBeNull()
+        {
+            //Arrange
+            Enemy newEnemy = new Enemy();
+
+
+            //Assert
+            Assert.Equal("Stefano", newEnemy.GetName());
+
+
+        }
+
+    }
+}
