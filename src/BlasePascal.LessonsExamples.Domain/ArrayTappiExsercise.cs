05a1cdf651178d9d3b6bddd7db8575c15a431c84
lorenzoostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2025-12-17 11:02:36 +0100

    Added Array exercise

---
+++ src/BlasePascal.LessonsExamples.Domain/ArrayTappiExsercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.LessonsExamples.Domain
+{
+    public class ArrayTappiExsercise
+    {
+        public int[] Array { get; private set; }
+
+        public ArrayTappiExsercise(int[] array)
+        {
+            Array = array;
+        }
+
+        public bool ContainsAllNumber()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (!Array.Contains(i)) 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
