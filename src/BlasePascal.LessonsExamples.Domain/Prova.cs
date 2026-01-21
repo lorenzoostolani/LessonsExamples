3a8e7fea8c67a09d3d682f6f4fcb49c1431edbe2
lorenzo.ostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2026-01-21 10:59:49 +0100

    Added record stuff

--- src/BlasePascal.LessonsExamples.Domain/Prova.cs
+++ src/BlasePascal.LessonsExamples.Domain/Prova.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlaisePascal.LessonsExamples.Domain
 {
-    public record Prova
+    public class Lamp
     {
+        public Guid Id { get; init; }
+        public int Intensity { get; private set; }
+        public Color Color { get; private set; }
+
+        public Lamp()
+        {
+            Id = Guid.NewGuid();
+            Color = new Color(255, 255, 255);
+        }
+
+        public void ChangeColorTo(Color newColor)
+        {
+            Color = newColor;
+            Color.red = 0;
+            Color = new Color(0, 0, 0);
+        }
+
+
     }
+
+    public sealed record Color(int red, int green, int blue);
 }
