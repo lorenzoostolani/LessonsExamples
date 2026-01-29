c7d87dbda158fde7fc77e1f72d68cdbd8f7a77fa
lorenzoostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2026-01-29 11:51:48 +0100

    SubMatrix method

---
+++ src/BlasePascal.LessonsExamples.Domain/Matrix/Matrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.LessonsExamples.Domain.Matrix
+{
+    public class Matrix
+    {
+
+        protected double[] vector;
+        private int rows, columns;
+
+        public Matrix(int numRows, int numColumns)
+        {
+            rows = Math.Abs(numRows);
+            columns = Math.Abs(numColumns);
+
+            // Controllo che la matrice non abbia dimensioni nulle
+            if ((rows * columns) == 0)
+                throw new Exception($"Impossible to create a {rows} * {columns} matrix.");
+
+            vector = new double[rows * columns];
+            for (int i = 0; i < vector.Length; i++)
+                vector[i] = 0.0;
+        }
+
+        public int Rows => rows;
+        public int Columns => columns;
+
+        public double this[int row, int column]
+        {
+            set
+            {
+                CheckDimensions(row, column);
+                vector[row * this.columns + column] = value;
+            }
+            get
+            {
+                CheckDimensions(row, column);
+                return vector[row * this.columns + column];
+            }
+        }
+
+        protected void CheckDimensions(int row, int column)
+        {
+            if (!((row >= 0) && (row < rows) && (column >= 0) && (column < columns)))
+            {
+                throw new OverflowException($"Impossible to access cell [{row}] [{column}].");
+            }
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder result = new System.Text.StringBuilder("", 1000);
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if ((i > 0) && ((i % columns) == 0))
+                    result.Append('\n');
+                result.AppendFormat("{0,11:f4}", vector[i]);
+            }
+            result.Append("\n");
+            return result.ToString();
+        }
+    }
+}
---
+++ src/BlasePascal.LessonsExamples.Domain/Matrix/SquareMatrix.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.LessonsExamples.Domain.Matrix
+{
+    public class SquareMatrix : Matrix
+    {
+        public SquareMatrix(int dimension) : base(dimension, dimension)
+        { }
+
+        public SquareMatrix(double[] original) : base((int)Math.Sqrt(original.Length), (int)Math.Sqrt(original.Length))
+        {
+            int end = vector.Length;
+            for (int i = 0; i < end; i++)
+                vector[i] = original[i];
+        }
+
+        public SquareMatrix() : this(3)
+        { }
+
+        public int Dimension => Rows;
+
+        public double Determinant(SquareMatrix matrix)
+        {
+            // Restituisce il determinante della matrice.
+            //
+            // 'matrix': matrice quadrata, di dimensione almeno 2.
+            //
+
+            int dim = matrix.Dimension;   // Dimensione matrice di input.
+            double result = 0;
+
+            if (dim == 2)
+            {
+                result = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            }
+            else
+            {
+                int r = 0;   // Riga sulla quale si calcola il determinante.
+                int k = 0;   // +1 o -1, coefficiente.
+
+                for (int c = 0; c < dim; c++)
+                {
+                    // Calcolo del segno basato sulla posizione (r+c)
+                    if ((r + c) % 2 == 0)
+                        k = 1;
+                    else
+                        k = -1;
+
+                    // Espansione di Laplace: elemento * segno * determinante della sottomatrice
+                    result += k * matrix[r, c] * Determinant(SubMatrix(matrix, r, c));
+                }
+            }
+
+            return result;
+        }
+
+        public SquareMatrix SubMatrix(SquareMatrix matrix, int row, int col)
+        {
+            SquareMatrix subMatrix = new SquareMatrix(matrix.Rows-1);
+            int r= 0;
+            int c = 0;
+            for (int j = 0; j<Rows; j++)
+            {
+                if (row == j)
+                    continue;
+
+                r++;
+                for (int k = 0; k < Rows; k++)
+                {
+                    if (col == k)
+                        continue;
+                    c++;
+                    subMatrix[r, c] = matrix[j, k];
+
+                }
+            }
+            return subMatrix;
+        }
+    }
+
+}
